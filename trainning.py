901dd40100e04dceaa19b7e336bd6a8ced651348
Wei Tong <knaw0218@cml18>
2023-01-16 22:35:44 +0800

    Add ploting

--- trainning.py
+++ trainning.py
@@ -11,6 +11,8 @@ from spektral.data import BatchLoader
 from model_NAS import ECC_Net, GIN_Net 
 from tqdm import tqdm
 import tensorflow_addons as tfa
+import matplotlib.pyplot as plt
+
 os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 ################################################################################
 # Config
@@ -45,6 +47,8 @@ def args_parse():
     args = parser.parse_args()
     return args
 
+# Loss(x, batch[]) =  abs ( (output(x) - output(batch[])) /(label(x) - label(batch[])) -1 )
+# batch[] is a random sample a data from the batch
 def K_rank(data): # input is an list of pair, containing prediction and true value
     n = len(data)
     ans = []
@@ -63,8 +67,7 @@ def K_rank(data): # input is an list of pair, containing prediction and true val
 
     return [np.mean(ans), np.std(ans), np.max(ans), np.min(ans)]
 
-
-def MAP(data): # input is an list of pair, containing prediction and true value
+def MAP(data, K): # input is an list of pair, containing prediction and true value
     ret = []
     for k in range(1000):
         inputs = data.copy()
@@ -81,13 +84,12 @@ def MAP(data): # input is an list of pair, containing prediction and true value
         pred_rank = [data[2] for data in inputs]
 
         ans = 0.0
-        for i in range(1,n+1):
-            now = 0.0
-            for j in pred_rank:
-                if j in true_rank[:i]:
-                    now += 1
-            ans += now / i
-        ret.append( ans / n )
+        now = 0.0
+        for i, j in enumerate(pred_rank):
+            if j in true_rank[:min(n, K)]:
+                now += 1
+                ans += now / (i+1)
+        ret.append( ans / now )
     return [np.mean(ret), np.std(ret), np.max(ret), np.min(ret)]
 
 def NDCG(all): # input is an list of pair, containing prediction and true value
@@ -161,18 +163,23 @@ idx_te = idx_all[13]
 ################################################################################
 # Fit model
 ################################################################################
-if args.do_train:
 
-    now_idx = np.array([],dtype="int64")
-    for dataset_len in range(13):
-        now_idx = np.concatenate((now_idx, idx_trs[dataset_len]),dtype=idx_trs[dataset_len].dtype)
-        dataset_tr = dataset[now_idx]
-        loader_tr = BatchLoader(dataset_tr, batch_size=args.batch_size, mask=True)
-        
-        model = GIN_Net()
-        optimizer = tf.keras.optimizers.Adam(args.lr)
-        model.compile(optimizer=optimizer, loss="mse") # loss = tfa.losses.TripletHardLoss()
-        checkpoint_filepath = './checkpoint/'+args.dataset+"_datasize_"+str(1000*(dataset_len+1))
+K_ranks = []
+NDCGs   = []
+MAPs    = []
+
+now_idx = np.array([],dtype="int64")
+for dataset_len in range(13):
+    now_idx = np.concatenate((now_idx, idx_trs[dataset_len]),dtype=idx_trs[dataset_len].dtype)
+    dataset_tr = dataset[now_idx]
+    loader_tr = BatchLoader(dataset_tr, batch_size=args.batch_size, mask=True)
+    
+    model = GIN_Net()
+    optimizer = tf.keras.optimizers.Adam(args.lr)
+    model.compile(optimizer=optimizer, loss="mse") # loss = tfa.losses.TripletHardLoss()
+    checkpoint_filepath = './checkpoint/'+args.dataset+"_datasize_"+str(1000*(dataset_len+1))
+
+    if args.do_train:
         model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
             filepath=checkpoint_filepath,
             save_weights_only=True,
@@ -192,33 +199,56 @@ if args.do_train:
         model.fit(loader_tr.load(), steps_per_epoch=loader_tr.steps_per_epoch, 
                     epochs=args.num_epoch, callbacks=[model_checkpoint_callback, early_stop_callback])
 
-# ################################################################################
-# # Evaluate model
-# ################################################################################
-model.load_weights(checkpoint_filepath).expect_partial()
-print("Testing model")
-loader_te = BatchLoader(dataset_te, batch_size=1, mask=True)
-loss = model.evaluate(loader_te.load(), steps=loader_te.steps_per_epoch)
-print("Done. Test loss: {}".format(loss))
+    ######################################################################
+    # Evaluating Model & Making Prediction
+    ######################################################################
+    dataset_te = dataset[idx_te]
+    loader_te = BatchLoader(dataset_te, batch_size=args.batch_size, mask=True)
+    model.load_weights(checkpoint_filepath).expect_partial()
+    print("Testing model")
+    loss = model.evaluate(loader_te.load(), steps = loader_te.steps_per_epoch)
+    loader_me = BatchLoader(dataset_te, batch_size = 1, epochs=1, mask=True, shuffle=False)
+    metric_input = []
+    progress = tqdm(total = len(dataset_te))
+    for data in loader_me.load():
+        metric_input.append([model.predict(data[0], verbose=0)[0][0], data[1][0][0]])
+        progress.update(1)
 
-loader_me = BatchLoader(dataset_te, batch_size=1, epochs=1, mask=True, shuffle=False)
+    print("K rank : ")
+    K_ranks.append(K_rank(metric_input))
+    print(K_ranks[-1])
+    print("MAP@10 : ")
+    MAPs.append(MAP(metric_input, 10))
+    print(MAPs[-1])
+    print("NDCG : ")
+    NDCGs.append(NDCG(metric_input))
+    print(NDCGs[-1])
 
-metric_input = []
-progress = tqdm(total=len(dataset_te))
-for i,data in enumerate(loader_me.load()):
-    
-    # print(data[0])
-    # print(all_label[i])
-    # print(all_pred[i])
+train_size = [ i*1000 for i in range(1, 14) ]
+# plot K_rank
+plt.figure(1)
+plt.plot(train_size, [data[0] for data in K_ranks])
+plt.plot(train_size, [data[1] for data in K_ranks])
+plt.plot(train_size, [data[2] for data in K_ranks])
+plt.plot(train_size, [data[3] for data in K_ranks])
+plt.savefig("K_rank.png")
 
-    # print(model.predict(data[0])[0][0])
-    # print(data[1][0][0])
-    metric_input.append([model.predict(data[0], verbose=0)[0][0], data[1][0][0]])
-    progress.update(1)
-    # assert(0)
-print(K_rank(metric_input))
-print(MAP(metric_input))
-print(NDCG(metric_input))
+plt.figure(2)
+plt.plot(train_size, [data[0] for data in MAPs])
+plt.plot(train_size, [data[1] for data in MAPs])
+plt.plot(train_size, [data[2] for data in MAPs])
+plt.plot(train_size, [data[3] for data in MAPs])
+plt.savefig("MAP.png")
+
+plt.figure(3)
+plt.plot(train_size, [data[0] for data in NDCGs])
+plt.plot(train_size, [data[1] for data in NDCGs])
+plt.plot(train_size, [data[2] for data in NDCGs])
+plt.plot(train_size, [data[3] for data in NDCGs])
+plt.savefig("NDCG.png")
+
+
+
 
 
 
